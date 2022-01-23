063b4024aedda3dc7af9fa6b20a44590318d0a31
PadraigF95 <44588037+PadraigF95@users.noreply.github.com>
2022-01-23 19:48:34 +0000

    mobile issue resolved

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
     const [colorTheme, setTheme] = useDarkMode()
   return (
-      <div className="grid grid-cols-2 justify-evenly w-full">
+      <div className="grid grid-cols-2 justify-evenly w-full sm:hidden">
           <Link to="/" className="pl-4">
                 <button id="home-button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded md:px-8">Home</button>
             </Link>
